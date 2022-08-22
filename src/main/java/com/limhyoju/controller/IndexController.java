9ff14de4132a6d2a6a14f604cebd0ad685ead024
HyojuLim <dlagywn94@gmail.com>
2022-08-22 14:59:18 +0900

    스프링 XML로 설정 연습

---
+++ src/main/java/com/limhyoju/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.limhyoju.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("data", "Limhyoju");
+		mv.setViewName("index");
+				
+		return mv ;
+	}
+}
